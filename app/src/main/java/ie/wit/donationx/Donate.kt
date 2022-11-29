2f84d2a514598bf07346c3d878728c8ff2008ac6
Decodon <decodon8@gmail.com>
2022-11-29 20:08:40 +0000

    Making a Donation - Part 1

--- app/src/main/java/ie/wit/donationx/Donate.kt
+++ app/src/main/java/ie/wit/donationx/Donate.kt
@@ -2,10 +2,40 @@ package ie.wit.donationx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ie.wit.donationx.databinding.ActivityDonateBinding
 
 class Donate : AppCompatActivity() {
+
+    private lateinit var donateLayout : ActivityDonateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donate)
+        donateLayout = ActivityDonateBinding.inflate(layoutInflater)
+        setContentView(donateLayout.root)
+
+        donateLayout.progressBar.max = 10000
+
+        donateLayout.amountPicker.minValue = 1
+        donateLayout.amountPicker.maxValue = 1000
+
+        donateLayout.amountPicker.setOnValueChangedListener { _, _, newVal ->
+            //Display the newly selected number to paymentAmount
+            donateLayout.paymentAmount.setText("$newVal")
+        }
+
+        var totalDonated = 0
+
+        donateLayout.donateButton.setOnClickListener {
+            val amount = if (donateLayout.paymentAmount.text.isNotEmpty())
+                donateLayout.paymentAmount.text.toString().toInt() else donateLayout.amountPicker.value
+            if(totalDonated >= donateLayout.progressBar.max)
+                Toast.makeText(applicationContext,"Donate Amount Exceeded!",Toast.LENGTH_LONG).show()
+            else {
+                totalDonated += amount
+                donateLayout.totalSoFar.text = getString(R.string.totalSoFar,totalDonated)
+                donateLayout.progressBar.progress = totalDonated
+            }
+        }
     }
 }
