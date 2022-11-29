b064c507ca21570da44b85f29c89514a273ce4be
Decodon <decodon8@gmail.com>
2022-11-29 20:57:59 +0000

    Switching between Activities with Menus

--- app/src/main/java/ie/wit/donationx/activities/Donate.kt
+++ app/src/main/java/ie/wit/donationx/activities/Donate.kt
@@ -1,7 +1,10 @@
 package ie.wit.donationx.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import ie.wit.donationx.R
 import ie.wit.donationx.databinding.ActivityDonateBinding
@@ -49,4 +52,24 @@ class Donate : AppCompatActivity() {
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_donate, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_report -> { startActivity(Intent(this, Report::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
