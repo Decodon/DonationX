0909282574fa22e712e218876e4190e1b599f76d
Decodon <decodon8@gmail.com>
2022-11-29 21:09:14 +0000

    Displaying our Donations List

--- app/src/main/java/ie/wit/donationx/activities/Report.kt
+++ app/src/main/java/ie/wit/donationx/activities/Report.kt
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import ie.wit.donationx.R
+import ie.wit.donationx.adapters.DonationAdapter
+import ie.wit.donationx.databinding.ActivityReportBinding
+import ie.wit.donationx.main.DonationXApp
+
 
 class Report : AppCompatActivity() {
+    lateinit var app: DonationXApp
+    lateinit var reportLayout : ActivityReportBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
+        reportLayout = ActivityReportBinding.inflate(layoutInflater)
+        setContentView(reportLayout.root)
+
+        app = this.application as DonationXApp
+        reportLayout.recyclerView.layoutManager = LinearLayoutManager(this)
+        reportLayout.recyclerView.adapter = DonationAdapter(app.donationsStore.findAll())
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
